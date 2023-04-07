27e3d14290eda7e9896e1f759df5659a88e98d0b
Tyler <tgoodwyn@gmail.com>
2023-04-06 19:25:19 -0500

    styled the page, added remove btn

--- Assessments/5_ Meme generator/app/app.js
+++ Assessments/5_ Meme generator/app/app.js
@@ -6,7 +6,7 @@ const submission = document.querySelector("#submission")
 const imgInput = document.querySelector("#meme-image-url")
 const topInput = document.querySelector("#meme-top-text")
 const bottomInput = document.querySelector("#meme-bottom-text")
-const meme_container = document.querySelector("#meme-container")
+const meme_container = document.querySelector(".meme-container")
 
 submission.addEventListener("submit", function (event) {
     event.preventDefault()
@@ -16,8 +16,21 @@ submission.addEventListener("submit", function (event) {
     const bottom_text = bottomInput.value
     add_meme_to_page(imgURL, top_text, bottom_text)
     console.log(`my imgURL is ${imgURL}`)
+    clear_values()
 })
 
+meme_container.addEventListener("click", function (e) {
+    e.preventDefault()
+    if (e.target.id === "remove_button") {
+        e.target.parentNode.remove()
+    }
+})
+
+function clear_values() {
+    imgInput.value = "" 
+    topInput.value = "" 
+    bottomInput.value = ""
+}
 
 function add_meme_to_page(imgURL, top_text, bottom_text) {
     const new_meme = document.createElement("div")
@@ -25,6 +38,17 @@ function add_meme_to_page(imgURL, top_text, bottom_text) {
     const new_img = document.createElement("img")
     new_img.setAttribute("src", imgURL)
     new_img.classList.add("meme-img")
+    
+
+    const rm_btn = document.createElement("button")
+    rm_btn.id = "remove_button"
+    rm_btn.classList.add("meme-btn")
+    rm_btn.innerText = "remove"
     new_meme.append(new_img)
+    new_meme.append(rm_btn)
+
     meme_container.append(new_meme)
 }
+
+
+
