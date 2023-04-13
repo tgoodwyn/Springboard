0498cebeaafea8ec2fc2c69f18f9b5c16dcf08d5
Tyler <tgoodwyn@gmail.com>
2023-04-13 18:24:09 -0500

    demo + exercises added for OO unit

---
+++ Exercises/12_1 OOP/exercises.js
@@ -0,0 +1,53 @@
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make
+        this.model = model
+        this.year = year
+    }
+
+    honk() {
+        return "Beep!"
+    }
+
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`
+    }
+
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make,model,year)
+        this.numWheels = 4
+    }
+
+
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make,model,year)
+        this.numWheels = 2
+    }
+
+    revEngine() {
+        return "VROOM!"
+    }
+
+}
+
+class Garage {
+
+    constructor(capacity) {
+        this.capacity = capacity
+        this.vehicles = []
+    }
+
+    add(vehicle) {
+        if (!(vehicle instanceof Vehicle)) return "Only vehicles here"
+        if (this.vehicles.length >= this.capacity) return "Sorry, at capcity."
+        this.vehicles.push(vehicle)
+        return "Vehicle added!";
+    }
+
+}
