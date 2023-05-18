7f9e112237a24a6981eecfb607b37eb89abe808a
Tyler <tgoodwyn@gmail.com>
2023-05-18 18:05:11 -0500

    prepared for code review

--- Projects/Fruit search/Workspace/script.js
+++ Projects/Fruit search/Workspace/script.js
@@ -6,30 +6,32 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 
 function search(str) {
 	let results = [];
-
+	for (const fruit_name of fruit) {
+		const fruit_name_lc = fruit_name.toLowerCase()
+		if (fruit_name_lc.includes(str)) {
+			results.push(fruit_name)
+		}
+	}
 	return results;
 }
 
 function searchHandler(e) {
 	const current_string = input.value.toLowerCase()
+	// this statement is necessary to not display anything when no text has yet been entered
 	if (!current_string) {
 		suggestions.innerHTML = ''
 		suggestions.classList.remove('has-suggestions')
 		 return;
 	}
-	const results = []
-	for (const fruit_name of fruit) {
-		const fruit_name_lc = fruit_name.toLowerCase()
-		if (fruit_name_lc.includes(current_string)) {
-			results.push(fruit_name)
-		}
-	}
+	const results = search(current_string)
+	
 	showSuggestions(results, current_string)
 }
 
 function showSuggestions(results, inputVal) {
 	
-	suggestions.innerHTML = ''
+	// this resets the suggestion display each time
+	suggestions.innerHTML = '' 
 	suggestions.classList.add('has-suggestions')
 	for (const result of results) {
 		const result_li = document.createElement("li")
@@ -38,10 +40,18 @@ function showSuggestions(results, inputVal) {
 	}
 }
 
+let evt = ''
 function useSuggestion(e) {
-	// TODO
+	//practicing using event delegation
+	evt = e
+	if (e.target.tagName === 'LI') { // remember that this needs to be all caps
 	const selected = e.target.innerText;
 	input.value = selected;
+	//need to clear the suggestion box after
+	suggestions.innerHTML = '' 
+	suggestions.classList.remove('has-suggestions')
+	// why is cursor showing up??
+	}
 }
 
 input.addEventListener('keyup', searchHandler);
